8905dc36f475f79f9edcdb97f48b7d2a1928da0e
Klemens Edler <klemens.edler@gmail.com>
2014-04-24 16:13:46 +0200

    Added table example

--- org.lunifera.example.vaadin.osgi/org.lunifera.example.vaadin.databinding/src/org/lunifera/example/vaadin/databinding/samples/SelectionBindings.java
+++ org.lunifera.example.vaadin.osgi/org.lunifera.example.vaadin.databinding/src/org/lunifera/example/vaadin/databinding/samples/SelectionBindings.java
@@ -5,14 +5,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.databinding.DataBindingContext;
+import org.lunifera.runtime.web.vaadin.components.fields.NumberField;
 import org.lunifera.runtime.web.vaadin.databinding.VaadinObservables;
 
 import com.vaadin.data.Container;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.ListSelect;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
 
 public class SelectionBindings extends CssLayout {
 
@@ -22,6 +26,10 @@ public class SelectionBindings extends CssLayout {
 	private CheckBox enabled;
 	private ListSelect list1;
 	private ListSelect list2;
+	private Table table;
+	private TextField text1;
+	private TextField text2;
+	private TextField text3;
 
 	public SelectionBindings() {
 		setSizeFull();
@@ -45,7 +53,7 @@ public class SelectionBindings extends CssLayout {
 		layout.addComponent(readonly, "top:25px;left:800px");
 		layout.addComponent(enabled, "top:25px;left:900px");
 
-		// Text fields
+		// lists
 		list1 = new ListSelect();
 		list1.setMultiSelect(true);
 		list1.setValue(new ArrayList<>());
@@ -77,14 +85,73 @@ public class SelectionBindings extends CssLayout {
 		dbc.bindValue(VaadinObservables.observeEnabled(list2),
 				VaadinObservables.observeValue(enabled));
 
-		Container ds = list1.getContainerDataSource();
-		ds.addItem("Row 1");
-		ds.addItem("Row 2");
-		ds.addItem("Row 3");
-		ds.addItem("Row 4");
-		ds.addItem("Row 5");
-		ds.addItem("Row 6");
-		ds.addItem("Row 7");
+		Container ds1 = list1.getContainerDataSource();
+		ds1.addItem("Row 1");
+		ds1.addItem("Row 2");
+		ds1.addItem("Row 3");
+		ds1.addItem("Row 4");
+		ds1.addItem("Row 5");
+		ds1.addItem("Row 6");
+		ds1.addItem("Row 7");
+
+		// table
+		table = new Table();
+		table.setImmediate(true);
+		table.setHeight("300px");
+		table.setSelectable(true);
+		table.setWidth("400px");
+		table.setPageLength(7);
+		table.setPropertyDataSource(new ObjectProperty<Bar>(null, Bar.class));
+		layout.addComponent(table, "top:300px;left:50px");
+
+		BeanItemContainer<Bar> ds2 = new BeanItemContainer<>(Bar.class);
+		ds2.addBean(new Bar("Joe", 1234, new Foo("Jackman")));
+		ds2.addBean(new Bar("Frank", 2345, new Foo("Falkner")));
+		ds2.addBean(new Bar("Alice", 3456, new Foo("Aaronson")));
+		ds2.addBean(new Bar("Bob", 4567, new Foo("Builder")));
+		ds2.addBean(new Bar("Eve", 6789, new Foo("Evans")));
+		ds2.addBean(new Bar("Marvin", 7777, new Foo("Miller")));
+		ds2.addBean(new Bar("Tim", 99999, new Foo("Taylor")));
+		table.setContainerDataSource(ds2);
+		table.setVisibleColumns("name", "number");
+
+		// Text fields
+		text1 = new TextField("Bar#name");
+		text1.setNullRepresentation("");
+		text1.setImmediate(true);
+		text1.setPropertyDataSource(new ObjectProperty<String>(null,
+				String.class));
+		text2 = new NumberField("Bar#number");
+		text2.setNullRepresentation("");
+		text2.setImmediate(true);
+		text2.setPropertyDataSource(new ObjectProperty<Integer>(0,
+				Integer.class));
+		text3 = new TextField("Bar -> Foo#name");
+		text3.setNullRepresentation("");
+		text3.setImmediate(true);
+		text3.setPropertyDataSource(new ObjectProperty<String>(null,
+				String.class));
+
+		layout.addComponent(text1, "top:300px;left:800px");
+		layout.addComponent(text2, "top:350px;left:800px");
+		layout.addComponent(text3, "top:400px;left:800px");
+
+		// bind the selection of list1 to list2 and vise verse
+		dbc.bindValue(VaadinObservables.observeSingleSelectionDetailValue(
+				table, Bar.class, "name"), VaadinObservables
+				.observeValue(text1));
+		dbc.bindValue(VaadinObservables.observeSingleSelectionDetailValue(
+				table, Bar.class, "number"), VaadinObservables
+				.observeValue(text2));
+		dbc.bindValue(VaadinObservables.observeSingleSelectionDetailValue(
+				table, Bar.class, "myfoo.name"), VaadinObservables
+				.observeValue(text3));
+
+		// bind readonly and enabled
+		dbc.bindValue(VaadinObservables.observeReadonly(table),
+				VaadinObservables.observeValue(readonly));
+		dbc.bindValue(VaadinObservables.observeEnabled(table),
+				VaadinObservables.observeValue(enabled));
 
 	}
 
